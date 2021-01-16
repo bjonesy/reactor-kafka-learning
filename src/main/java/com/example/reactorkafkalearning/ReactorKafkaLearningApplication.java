ee35982f3777058767ea7b1e2cd572e51533493b
Brandon Jones <brandon.jones1985@gmail.com>
2021-01-15 22:32:04 -0500

    Initial commit

---
+++ src/main/java/com/example/reactorkafkalearning/ReactorKafkaLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactorkafkalearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorKafkaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorKafkaLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactorkafkalearning/ReactorKafkaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactorkafkalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorKafkaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
